c838ed5fc74f95c66268104bf10d6cea35bed2e6
Kory Hoopes <kory@hey.com>
2023-05-17 15:28:22 -0600

    Changing the build script to install packages asynchronously in groups
    You can now install scripts asynchronously in groups.

--- utilities/build.js
+++ utilities/build.js
@@ -5,43 +5,58 @@ const syncCommands = [
         name: 'removeDist',
         command: 'rm -rf ./dist',
         message: 'The ./dist folder has been deleted.'
-    },
-    {
-        name: 'runTSC',
-        command: 'tsc',
-        message: 'The TypeScript compiler has run successfully.'
-    },
-    {
-        name: 'runSASS',
-        command: 'sass ./src/public/css:./dist/public/css',
-        message: 'The SASS compiler has run successfully.'
     }
 ];
-const asyncCommands = [
-    {
-        name: 'copyHTML',
-        command: 'cp -r ./src/public/html/* ./dist/public',
-        message: 'Successfully copied all HTML to the ./dist/public/html folder.'
-    },
-    {
-        name: 'copyVendorCSS',
-        command: 'cp ./node_modules/bulma/css/bulma.min.css ./dist/public/css',
-        message: 'Successfully copied all vendor CSS to the ./dist/public/css folder.'
-    }
-];
+const asyncCommands = {
+    group1: [
+        {
+            name: 'runTSC',
+            command: 'tsc',
+            message: 'The TypeScript compiler has run successfully.'
+        },
+        {
+            name: 'runSASS',
+            command: 'sass ./src/public/css:./dist/public/css',
+            message: 'The SASS compiler has run successfully.'
+        }
+    ],
+    group2: [
+        {
+            name: 'copyHTML',
+            command: 'cp -r ./src/public/html/* ./dist/public',
+            message: 'Successfully copied all HTML to the ./dist/public/html folder.'
+        },
+        {
+            name: 'copyVendorCSS',
+            command: 'cp ./node_modules/bulma/css/bulma.min.css ./dist/public/css',
+            message: 'Successfully copied all vendor CSS to the ./dist/public/css folder.'
+        }
+    ]
+};
+
+function runAsync(group) {
+    return Promise.all(
+        group.map(command => {
+            return new Promise(resolve => {
+                exec(command.command, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(error);
+                        return;
+                    }
+                    console.log(command.message);
+                    resolve('success');
+                });
+            })
+        })
+    );
+}
 
 syncCommands.forEach(config => {
     execSync(config.command);
-
     console.log(config.message);
 });
-asyncCommands.forEach(config => {
-    exec(config.command, (err) => {
-        if (err) {
-            console.error(`Error: ${err}`);
-            return;
-        }
-        
-        console.log(config.message);
-    });
-});
+
+for (const group in asyncCommands) {
+    await runAsync(asyncCommands[group]);
+    
+}
