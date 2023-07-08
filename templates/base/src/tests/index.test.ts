9da2e275e9f99f63b0c54b45d49f39e8ff55b515
Kory Hoopes <kory@seabear.dev>
2023-07-07 21:03:09 -0600

    Source formatting
    Cleaning up various files to be more efficient and easier to read.

--- templates/base/src/tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, jest, test } from '@jest/globals';
-import { index } from '../app/index.js';
-
-test('Sanity check by making sure the index constant from app/index.ts is defined.', (done) => {
-    setTimeout(() => {
-        expect(index).toBeDefined();
-        done();
-    });
-});
--- templates/web-server/src/tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@jest/globals';
-import { web } from '../app/index.js';
-
-test('Make sure the port and server properties are defined', (done) => {
-    setTimeout(() => {
-        expect(web.port).toBe(4242);
-        done();
-    });
-});
