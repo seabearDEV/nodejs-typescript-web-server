307db153a71a459f40152a30a7fb2e6da599ee90
Kory Hoopes <kory@seabear.dev>
2023-07-07 21:04:44 -0600

    Simplified genesis.js
    Rewrote most of genesis.js to be more efficient and easier to read. Even after this improvement it's still pretty slow on creating a web server application, I should look into running some of the commands asynchronously.

--- utilities/genesis.js
+++ utilities/genesis.js
@@ -2,42 +2,66 @@ import { cwd } from 'process';
 import { execSync } from 'child_process';
 import { readFile, writeFile } from 'fs/promises';
 
-const base = JSON.parse(await readFile(`${cwd()}/templates/base/base.json`));
-const packageJSON = JSON.parse(await readFile(`${cwd()}/package.json`));
+// Get the template type from the command line or use 'base' as a default
 const templateType = process.argv.slice(2)[0] ? process.argv.slice(2)[0] : 'base';
-const template = JSON.parse(await readFile(`${cwd()}/templates/${templateType}/${templateType}.json`));
-const dependencies = template['package-json'].dependencies;
-const devDependencies = template['package-json'].devDependencies;
-const scripts = template['package-json'].scripts;
 
+// Get the current package.json file
+const packageJSON = JSON.parse(await readFile(`${cwd()}/package.json`));
+
+// Get the base and application templates
+const baseTemplate = JSON.parse(await readFile(`${cwd()}/templates/base/base.json`));
+const applicationTemplate = JSON.parse(await readFile(`${cwd()}/templates/${templateType}/${templateType}.json`));
+
+// Set the combined template constants
+const dependencies = {
+    baseTemplate: baseTemplate['package-json'].dependencies,
+    applicationTemplate: applicationTemplate['package-json'].dependencies
+};
+const devDependencies = {
+    baseTemplate: baseTemplate['package-json'].devDependencies,
+    applicationTemplate: applicationTemplate['package-json'].devDependencies
+};
+const scripts = {
+    baseTemplate: baseTemplate['package-json'].scripts,
+    applicationTemplate: applicationTemplate['package-json'].scripts
+};
+
+// Set the template-type property in the package.json object
 packageJSON['template-type'] = templateType;
-packageJSON.scripts = {};
-packageJSON.dependencies = {};
-packageJSON.devDependencies = {};
 
-for (const script in base['package-json']['scripts']) {
-    packageJSON.scripts[script] = base['package-json']['scripts'][script];
+// Install dependencies from templates
+function installDependencies(dependencies, templateItem, type) {
+    if (dependencies[templateItem]) {
+        dependencies[templateItem].forEach(dependency => {
+            execSync(`npm install ${dependency} ${type}`);
+        });
+    };
 }
 
-for (const script in scripts) {
-    packageJSON.scripts[script] = scripts[script];
+// Set scripts from templates into package.json
+function setScripts(templateItem) {
+    for (const script in scripts[templateItem]) {
+        packageJSON.scripts[script] = scripts[templateItem][script];
+    }
 }
 
+// Loop over all the scripts from templates and run the setScripts function
+for (const templateItem in scripts) { 
+    setScripts(templateItem);
+}
+
+// Write the new package.json file
 await writeFile(`${cwd()}/package.json`, JSON.stringify(packageJSON));
 
-if (devDependencies) {
-    devDependencies.forEach(dependency => {
-        execSync(`npm install --save-dev ${dependency}`);
-    });
+for (const templateItem in dependencies) {
+    installDependencies(dependencies, templateItem, '--save');
+}
+for (const templateItem in devDependencies) {
+    installDependencies(devDependencies, templateItem, '--save-dev');
 }
 
-if (dependencies) {
-    dependencies.forEach(dependency => {
-        execSync(`npm install --save ${dependency}`);
-    });
-}
-
-template['genesis-js'].syncCommands.forEach(command => {
+// Run the synchronous commands defined in the application template
+applicationTemplate['genesis-js'].syncCommands.forEach(command => {
     execSync(command.run);
     console.log(command.message);
 });
